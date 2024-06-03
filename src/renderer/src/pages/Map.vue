b7159df05030ca6abcfa884727a918e2fe59692d
Coffeesw <897884964@qq.com>
2024-06-04 00:17:00 +0800

    ✨ feat: ADD THREE

--- src/renderer/src/pages/Map.vue
+++ src/renderer/src/pages/Map.vue
@@ -1,12 +1,19 @@
 <template>
     <MainWrapper>
         <div main-conetnt>
-            <div size-full bg-active-blue></div>
+            <div ref="threeRef" size-full border border-solid border-gray></div>
         </div>
     </MainWrapper>
 </template>
 
 <script lang="ts" setup>
 import MainWrapper from '@renderer/pages/layouts/index.vue'
+import { onMounted, Ref, ref } from 'vue';
+const threeRef: Ref<null | HTMLCanvasElement> = ref(null)
+onMounted(() => {
 
+
+})
 </script>
+
+<style scoped></style>
